6d1bed0c397dc75613288936c1ab3a0aadb46ad6
Doon <h.dan.vuong@gmail.com>
2018-03-14 11:15:48 +0100

    version bonne detecction pic mais erreur
    La valeur max des pics n'est pas forcement la bonne valeur

--- Projets8/test_matlab.m
+++ Projets8/test_matlab.m
@@ -8,14 +8,15 @@ clc
 
 fe=10000;
 Ts=0.001;
-Ns=128*10;
 Nfft=512;
 xaxisf=-(fe/2):fe/Nfft:(fe/2)-(fe/Nfft);
 n=128; %nombre de sous porteuses
-l=32;%taille du préfixe cyclique
+l=16;%taille du préfixe cyclique
+N=10;
+Ns=n*N;
 
 % Bruit
-sigma2 = 0.001;
+sigma2 = 0.0000;
 moyenne = 0;
 bruit_r = moyenne + sqrt(sigma2/2)*randn(1,Ns+l*(Ns/n));
 bruit_i = moyenne + sqrt(sigma2/2)*randn(1,Ns+l*(Ns/n));
@@ -42,15 +43,22 @@ spc=[ssp(n-l+1:n),ssp];
 cpt=2;
 prefixes = zeros(length(ssp)/n, l);
 prefixes(1,:)=ssp(n-l+1:n);
+cpt_indice=2;
+indice_prefixe(1)=1;
 for j = 2:(length(ssp)/n);
     temp = ssp(j*n-l+1:j*n);
     spc_temp = [spc(1:(j-1)*n+(j-1)*l), temp];
     spc = [spc_temp , spc((j-1)*n+(j-1)*l+1:end)];
     prefixes(cpt,:)=temp;
+    indice_prefixe(cpt_indice)=(j-1)*n+(j-1)*l+1;
     cpt=cpt+1;
-    
+    cpt_indice = cpt_indice+1;
 end;
 
+%% calcul vrai indice
+for i = 1:N
+    vrai_indice_prefixe(i)=1+(n+l)*(i-1);
+end
 %P/S : pas besoin sur Matlab
 
 
@@ -68,7 +76,7 @@ cpt_mat = 1;
 cpt_max=1;
 test_pic=0;
 cpt_pos_max=1;
-prefixes_detect =zeros(length(ssp)/n, l);
+%prefixes_detect =zeros(length(ssp)/n, l);
 for i = 1:length(spc_b)-n-l+1;
   
     buffer = spc_b(i:i+n+l-1);
@@ -80,7 +88,7 @@ for i = 1:length(spc_b)-n-l+1;
 end;
 
 eps(length(spc_b)-n+1)=0;
- for j = 1:length(eps)
+ for j = 1:length(eps) % Detection des pics 
         if eps(j) > Err
             test_pic=1;
             buff_max(cpt_max)=eps(j);
@@ -92,7 +100,7 @@ eps(length(spc_b)-n+1)=0;
             M = max(buff_max);
             for k=1:length(buff_max)
                 if buff_max(k)==M
-                    pos_max(cpt_pos_max)=buff_indice(k);;
+                    pos_max(cpt_pos_max)=buff_indice(k);
                     cpt_pos_max=cpt_pos_max+1;
                 end
             end
@@ -100,7 +108,6 @@ eps(length(spc_b)-n+1)=0;
             buff_indice=[];
         end
  end
- 
  
  
 plot(eps)
@@ -115,9 +122,13 @@ for i = 1:length(pos_max)
     
 end
 
+hold all
+stem(pos_max,eps(pos_max));
+stem(indice_prefixe,eps(indice_prefixe),'x');
+
 %FFT
 
-B1 = ((fft(A.')));
+B1 = (sqrt(n)./(fft(A.')));
 
 
 %P -> S
@@ -132,6 +143,8 @@ B(B1 >= 0) = 1;
 B(B1 < 0) = -1;
 
 B=(B+1)/2;
+figure
+stem(B-x)
 
 %TEB
 
